e963d0eeccec2510418bf4652290b8f7e809fc68
gavin-addzero <gavin.addzero@gmail.com>
2011-10-25 00:17:42 +0800

    make it better

--- src/gavin/web/DefaultFilter.java
+++ src/gavin/web/DefaultFilter.java
@@ -1,8 +1,11 @@
 package gavin.web;
 
+import gavin.IOUtil;
 import gavin.Util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -23,32 +26,34 @@ public class DefaultFilter implements Filter {
 		application = config.getServletContext();
 		encoding = config.getInitParameter("encoding");
 		if (Util.isEmpty(encoding)) {
-			encoding = System.getProperty("file.encoding", "UTF-8");
+			encoding = Util.getEncoding();
 		}
 	}
 
 	@Override
-	public void destroy() {}
+	public void destroy() {
+	}
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		long cost = System.currentTimeMillis();
-		// following two lines should be removed. almost all sevlet container can setup default request and reponse encoding
 		req.setCharacterEncoding(encoding);
 		res.setCharacterEncoding(encoding);
-		// cast for future use
+		//
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
-		// for no cache
+		// avoid cache
 		response.setHeader("Cache-Control", "no-cache");
 		response.setHeader("Pragma", "no-cache");
 		response.setDateHeader("Expires", -1L);
-
-		// response.sendRedirect(response.encodeRedirectURL("/"));
-		// response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-
-		chain.doFilter(req, res);
 		//
-		application.log("Request process in " + (System.currentTimeMillis() - cost) + " milliseconds");
+		long cost = System.currentTimeMillis();
+		chain.doFilter(req, res);
+		cost = System.currentTimeMillis() - cost;
+		//
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		PrintStream output = new PrintStream(buffer);
+		IOUtil.copy(request.getInputStream(), output);
+		output.append("\ncost " + cost + " milliseconds").flush();
+		application.log(new String(buffer.toByteArray(), encoding));
 	}
 }
