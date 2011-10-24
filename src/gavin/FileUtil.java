4412c9af64d4a6bd5871e45655d9a4a9bd916962
gavin-addzero <gavin.addzero@gmail.com>
2011-10-25 00:10:45 +0800

    make accept method more useful

--- src/gavin/FileUtil.java
+++ src/gavin/FileUtil.java
@@ -18,7 +18,7 @@ public class FileUtil {
 	}
 
 	public static void delete(File file, FileFilter... filter) {
-		boolean delete = accept(file, filter);
+		boolean delete = accept(file, true, filter);
 		if (file.isDirectory()) {
 			File[] children = file.listFiles();
 			if (children != null) {
@@ -38,18 +38,47 @@ public class FileUtil {
 		}
 	}
 
-	public static boolean accept(File file, FileFilter... filters) {
+	/**
+	 * 
+	 * @param file
+	 *            target file to filter
+	 * @param flag
+	 *            if true any accept than true else any not accept than false
+	 * @param filters
+	 *            the filters
+	 * @return
+	 */
+	public static boolean accept(File file, boolean flag, FileFilter... filters) {
 		for (FileFilter filter : filters) {
-			if (!filter.accept(file)) {
-				return false;
+			if (flag == filter.accept(file)) {
+				return flag;
 			}
 		}
-		return true;
+		return !flag;
 	}
 
 	public static String getExtention(File file) {
 		String name = file.getName();
 		int index = name.lastIndexOf('.');
 		return index != -1 ? name.substring(index + 1).toLowerCase() : null;
+	}
+
+	public static void main(String[] args) {
+		FileFilter a = new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				return pathname.getName().endsWith(".java");
+			}
+		};
+		FileFilter b = new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				return pathname.getName().startsWith("IO");
+			}
+		};
+		File f = new File("src/gavin");
+		for (File file : f.listFiles()) {
+			System.out.println(file.getName() + "=" + accept(file, true, a, b));
+		}
 	}
 }
